791d2996f265ce35275e6d88cec5d9c8d5635823
Deepti Verma <deepti.verma@vistaprint.com>
2023-05-03 00:27:16 +0530

    fixed file path

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import CartItem from "./cartItem";
+import CartItem from "./CartItem";
 
 const Cart = ()=>{
     const cartItems = useSelector(store=>store.cart.items)
